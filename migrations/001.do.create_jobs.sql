43bdc37944755cebe1b726a1d45683e639b6b0af
Arit Oyekan <aaoyekanmi4@gmail.com>
2020-02-20 17:25:54 -0600

    seeded jobs table

---
+++ migrations/001.do.create_jobs.sql
@@ -0,0 +1,13 @@
+CREATE TABLE jobs (
+  id SERIAL PRIMARY KEY,
+  company TEXT NOT NULL,
+  job_role TEXT NOT NULL,
+  job_location TEXT,
+  job_description TEXT,
+  found_at TEXT,
+  applied BOOLEAN,
+  phone_screen DATE,
+  interview DATE,
+  offer INTEGER,
+  date_created TIMESTAMP DEFAULT now() NOT NULL
+);
---
+++ seeds/seed.jobs.sql
@@ -0,0 +1,5 @@
+INSERT INTO jobs (company, job_role, job_location,applied)
+VALUES
+  ('Apple', 'Junior Web Developer', 'Austin, TX', TRUE),
+  ('Infosys', 'React Developer', 'Austin, TX', FALSE),
+  ('IBM', 'Front-End Developer','Austin, TX', TRUE)
