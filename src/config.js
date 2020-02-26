04a8c3d7e74bb1cca54b4b9a40ec63edfca59b2a
Arit Oyekan <aaoyekanmi4@gmail.com>
2020-02-26 17:25:30 -0600

    changed DB to DATABASE in env file

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
-    DATABASE_URL: process.env.DB_URL || 'postgresql://dunder_mifflin@localhost/job_tracker',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/job_tracker',
     SSL: !!process.env.SSL,
   }
