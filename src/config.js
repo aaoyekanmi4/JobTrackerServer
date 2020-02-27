51aeceab635cd8f9ff85e5a641267e43625b5dc3
Arit Oyekan <aaoyekanmi4@gmail.com>
2020-02-26 18:04:34 -0600

    added ssl to postgrator config

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/job_tracker',
-    SSL: !!process.env.SSL,
+    "ssl": !!process.env.SSL,
   }
