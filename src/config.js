785b90b3f23d0c488c459287bb93d720837137dd
Arit Oyekan <aaoyekanmi4@gmail.com>
2021-03-25 22:22:32 -0500

    changed config for ssl

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/job_tracker',
-    "ssl": !!process.env.SSL,
+  DATABASE_URL: process.env.DATABASE_URL`${'?ssl=true'}` || 'postgresql://dunder_mifflin@localhost/job_tracker',
+    "ssl": true,
   }
