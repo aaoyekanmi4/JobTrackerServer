68b1da3fa48b3ddbb22d4d6899f4589132f451f8
Arit Oyekan <aaoyekanmi4@gmail.com>
2021-03-25 21:54:29 -0500

    added cors options

--- src/app.js
+++ src/app.js
@@ -16,6 +16,7 @@ const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+app.options('*', cors());
 
 app.use('/api/auth', authRouter);
 app.use('/api/users', usersRouter);
