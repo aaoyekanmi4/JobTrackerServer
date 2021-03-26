b96e44749c12b1289ede4ed3e1c9356bc08aac14
Arit Oyekan <aaoyekanmi4@gmail.com>
2021-03-25 22:54:49 -0500

    changed ssl

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   DATABASE_URL: process.env.DATABASE_URL`${'?ssl=true'}` || 'postgresql://dunder_mifflin@localhost/job_tracker',
-    "ssl": true,
+    "ssl": !!process.env.ssl
   }
