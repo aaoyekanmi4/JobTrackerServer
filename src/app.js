46eaee1abf3fcdbbd9a4dd8aac74efa3415bc742
Arit Oyekan <aaoyekanmi4@gmail.com>
2021-04-13 14:02:29 -0500

    fixing cors() package

--- src/app.js
+++ src/app.js
@@ -16,7 +16,6 @@ const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
-app.options('*', cors());
 
 app.use('/api/auth', authRouter);
 app.use('/api/users', usersRouter);
